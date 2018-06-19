fb1e2e926ee0aaf29d628689a3c94de452396386
bcarlson01 <bcarlson@berkeley.edu>
2018-06-19 00:04:31 -0700

    Can display memes woooo

--- app/src/main/java/com/bemad/bcarlson/memer/MemeAdapter.java
+++ app/src/main/java/com/bemad/bcarlson/memer/MemeAdapter.java
@@ -1,21 +1,25 @@
 package com.bemad.bcarlson.memer;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 
 /**
  * Created by bcarlson on 6/12/18.
  */
 
-public class MemeAdapter extends ArrayAdapter<Meme> {
+public class MemeAdapter extends ArrayAdapter<Meme>{
     private Context context;
 
     public MemeAdapter(Context context, int resourceID, ArrayList<Meme> items) {
@@ -30,7 +34,33 @@ public class MemeAdapter extends ArrayAdapter<Meme> {
 
         ImageView image = convertView.findViewById(R.id.image);
         System.out.println("Meme URL: " + meme.getMemeUrl());
-        Glide.with(convertView.getContext()).load(meme.getMemeUrl()).into(image);
+        new DownloadImageTask(image).execute(meme.getMemeUrl());
+        //Glide.with(convertView.getContext()).load(meme.getMemeUrl()).into(image);
         return convertView;
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
